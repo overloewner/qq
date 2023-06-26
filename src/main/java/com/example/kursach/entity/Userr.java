f4baa8c79a8fec4b9ff0839c028e8f61f4713fe9
drago <dragovaen88@gmail.com>
2023-06-26 14:32:28 +0300

    'awwwww'

--- src/main/java/com/example/kursach/entity/Userr.java
+++ src/main/java/com/example/kursach/entity/Userr.java
@@ -2,19 +2,49 @@ package com.example.kursach.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Userr {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
+    private String post;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
     private String login;
     private String password;
-    public Userr(String login, String password) {
-        this.login = login;
-        this.password = password;
+
+    public Userr(String post, String name) {
+        this.post = post;
+        this.name = name;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public Userr() {
     }
 
